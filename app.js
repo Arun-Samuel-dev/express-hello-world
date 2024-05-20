cf1619da08453243dc59bc6c72deb713c022867b
Arun-Samuel-dev <150673519+Arun-Samuel-dev@users.noreply.github.com>
2024-05-20 21:25:07 +0530

    Added health end point

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/health", (req, res) => res.status(200).json({'we': 'are ok'}));
+
+app.get("/", (req, res) => res.type('html').send(html()));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-const html = `
+const html = () => `
 <!DOCTYPE html>
 <html>
   <head>
